ba48a6bcb18cabe14afc6d2497d221a8a99386d2
Kenn <koa10@aber.ac.uk>
2023-02-23 13:27:59 +0000

    link screen widgets to bottom nav bar items selection now moves to new screen
    Signed-off-by: Kenn <kakomea@yahoo.com>

---
+++ lib/screens/my_courses_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class MyCourses extends StatelessWidget {
+  const MyCourses({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('My courses'),
+    );
+  }
+}
