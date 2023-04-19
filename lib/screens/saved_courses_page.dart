64a1bae5c3bee20499846fd1d51e68be46dac23f
Kenn <kakomea@yahoo.com>
2023-04-19 03:52:55 +0100

    saved course tile now responds to onTop event
    Signed-off-by: Kenn <kakomea@yahoo.com>

--- lib/screens/saved_courses_page.dart
+++ lib/screens/saved_courses_page.dart
@@ -11,6 +11,7 @@ import 'package:provider/provider.dart';
 
 import '../constants/constants.dart';
 import '../models/course_data_model.dart';
+import '../routes/routes.dart';
 import '../shared_widgets/saved_course_card.dart';
 import '../utils/auth.dart';
 import '../utils/firebase_data_management.dart';
@@ -71,10 +72,10 @@ class _SavedCoursesState extends State<SavedCourses> {
     return db.getSavedCourses(savedCoursesNotifier, myDocId);
   }
 
-   Future<dynamic> _deleteDocument() async {
+  Future<dynamic> _deleteDocument() async {
     print('called');
     await db.test();
-    if(mounted){
+    if (mounted) {
       setState(() {
         futureData = getModels();
       });
@@ -83,7 +84,7 @@ class _SavedCoursesState extends State<SavedCourses> {
 
   @override
   void setState(fn) {
-    if(mounted) {
+    if (mounted) {
       super.setState(fn);
     }
   }
@@ -129,10 +130,12 @@ class _SavedCoursesState extends State<SavedCourses> {
                   return futureData;
                 },
                 child: SingleChildScrollView(
-                  child: SizedBox(
-                      width: double.infinity,
-                      height: MediaQuery.of(context).size.height * 0.80.w,
-                      child:   EmptyFavouritesPage(onAdd: _deleteDocument,),
+                    child: SizedBox(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.80.w,
+                  child: EmptyFavouritesPage(
+                    onAdd: _deleteDocument,
+                  ),
                 )));
           }
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -140,11 +143,17 @@ class _SavedCoursesState extends State<SavedCourses> {
               savedList: savedList,
               savedCoursesNotifier: savedCoursesNotifier,
               courseNotifier: courseNotifier,
-              db: db, onDelete: _deleteDocument,
+              db: db,
+              onDelete: _deleteDocument,
             );
           } else if (snapshot.connectionState == ConnectionState.done) {
-            return SavedCourseTile(savedList: savedList, savedCoursesNotifier: savedCoursesNotifier,
-              courseNotifier: courseNotifier, db: db, onDelete: _deleteDocument,);
+            return SavedCourseTile(
+              savedList: savedList,
+              savedCoursesNotifier: savedCoursesNotifier,
+              courseNotifier: courseNotifier,
+              db: db,
+              onDelete: _deleteDocument,
+            );
           } else if (snapshot.hasError) {
             return Text('Error: ${snapshot.error}');
           }
@@ -170,7 +179,8 @@ class SavedCourseTile extends StatefulWidget {
       required this.savedList,
       required this.savedCoursesNotifier,
       required this.courseNotifier,
-      required this.db, required this.onDelete})
+      required this.db,
+      required this.onDelete})
       : super(key: key);
 
   @override
@@ -186,53 +196,60 @@ class _SavedCourseTileState extends State<SavedCourseTile> {
           scrollDirection: Axis.vertical,
           itemCount: widget.savedList.length,
           itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.only(bottom: 10.0),
-              child: Slidable(
-                  key: Key(widget.savedList[index].courseId),
-                  // The start action pane is the one at the left or the top side.
-                  endActionPane: ActionPane(
-                    // A motion is a widget used to control how the pane animates.
-                    motion: const ScrollMotion(),
+            return GestureDetector(
+              onTap: (){
+                widget.courseNotifier.currentCourse = widget.savedList[index];
+                Navigator.pushNamed(
+                    context, PageRoutes.courseInfo);
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 10.0),
+                child: Slidable(
+                    key: Key(widget.savedList[index].courseId),
+                    // The start action pane is the one at the left or the top side.
+                    endActionPane: ActionPane(
+                      // A motion is a widget used to control how the pane animates.
+                      motion: const ScrollMotion(),
 
-                    // A pane can dismiss the Slidable.
-                    dismissible: DismissiblePane(
-                        onDismissed: () {
-                          widget.onDelete.call();
-                      _handleRemoveSavedCourse(index);
-                    } ),
+                      // A pane can dismiss the Slidable.
+                      dismissible: DismissiblePane(onDismissed: () {
+                        widget.onDelete.call();
+                        _handleRemoveSavedCourse(index);
+                      }),
 
-                    // All actions are defined in the children parameter.
-                    children: [
-                      // A SlidableAction can have an icon and/or a label.
-                      SlidableAction(
-                        autoClose: true,
-                        onPressed: (context) {},
-                        backgroundColor: const Color(0xFF21B7CA),
-                        foregroundColor: Colors.white,
-                        icon: Icons.share,
-                        label: 'Share',
-                      ),
+                      // All actions are defined in the children parameter.
+                      children: [
+                        // A SlidableAction can have an icon and/or a label.
+                        SlidableAction(
+                          autoClose: true,
+                          onPressed: (context) {},
+                          backgroundColor: const Color(0xFF21B7CA),
+                          foregroundColor: Colors.white,
+                          icon: Icons.share,
+                          label: 'Share',
+                        ),
 
-                      SlidableAction(
-                        autoClose: true,
-                        onPressed: (context) => _handleRemoveSavedCourse(index),
-                        backgroundColor: const Color(0xFFFE4A49),
-                        foregroundColor: Colors.white,
-                        icon: Icons.delete,
-                        label: 'Delete',
-                      ),
-                    ],
-                  ),
-                  child: SavedCourseCard(
-                    displayList: widget.savedList,
-                    index: index,
-                  )),
+                        SlidableAction(
+                          autoClose: true,
+                          onPressed: (context) => _handleRemoveSavedCourse(index),
+                          backgroundColor: const Color(0xFFFE4A49),
+                          foregroundColor: Colors.white,
+                          icon: Icons.delete,
+                          label: 'Delete',
+                        ),
+                      ],
+                    ),
+                    child: SavedCourseCard(
+                      displayList: widget.savedList,
+                      index: index,
+                    )),
+              ),
             );
           }),
     );
   }
-   _handleRemoveSavedCourse(int index) {
+
+  _handleRemoveSavedCourse(int index) {
     widget.courseNotifier.currentCourse = widget.savedList[index];
     widget.db.removeSavedCourseSubCollection(
       savedCourses: widget.savedCoursesNotifier,
